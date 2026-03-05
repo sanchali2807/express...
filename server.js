4fe8d1ec1b10e6bdb55b42d59541c649fa1ef3b5
Sanchali <sanchali280705@gmail.com>
2026-03-05 22:06:27 +0530

    static routing

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 // import express from 'express';
 const express = require('express');
+const path = require('path');
 const app = express();
 //this app is basically used for everything 
 const port = 8080;
 
-app.get('/',(req,res)=>{
-     res.send('<h1>Hello world</h1>');
-})
-app.get('/about',(req,res)=>{
-    res.send('this is about');
-})
+//setup static folder -- is a middleware
+app.use(express.static(path.join(__dirname,'public')));
+
+// app.get('/',(req,res)=>{
+//     //  res.send('<h1>Hello world</h1>');
+//     res.sendFile(path.join(__dirname,'public','index.html'));
+// })
+// app.get('/about',(req,res)=>{
+//     // res.send('this is about');
+//     res.sendFile(path.join(__dirname,'public','about.html'));
+// })
 
 app.listen(port,()=>{
     console.log(`server is running ${port}`);
